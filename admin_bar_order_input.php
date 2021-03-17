0ef00a2d2eddccff4e564580f772d60dfa125c5f
Natalya <Lady-Natalya@users.noreply.github.com>
2021-03-17 10:26:23 -0400

    Update admin_bar_order_input.php

--- admin_bar_order_input.php
+++ admin_bar_order_input.php
@@ -1,16 +1,16 @@
-// V 1.0.0
+// V 1.0.1
 // Creates a box at the top of the Admin view where one can type in an order (or post) ID and be taken to its edit page
 
 add_action('admin_bar_menu', 'add_toolbar_items', 100);
 function add_toolbar_items($admin_bar){
+  
 	$admin_bar->add_menu( array(
 		'id'    => 'order-input',
 		'title' => '<input id="orderlookup"
-			onClick="console.log(orderlookup.value, orderlookup.value.length);
-			if (orderlookup.value.length > 0) {
-				window.location = \'https://mosaicartsupply.com/wp-admin/post.php?post=\' + orderlookup.value + \'&action=edit\';
-			}
-			"
+			onClick="if (orderlookup.value.length > 0) {
+				let sanitizedVal = orderlookup.value.replace(/\D/g, \'\');
+				window.location = \'https://mosaicartsupply.com/wp-admin/post.php?post=\' + sanitizedVal + \'&action=edit\';
+			}"
 			class="testclass"
 			placeholder="Go to Order #"
 			style="display:inline-block; min-width:6rem; padding:0 0.5rem; background-color:white; border:none; box-shadow: inset 0 0 2px #000000;"
